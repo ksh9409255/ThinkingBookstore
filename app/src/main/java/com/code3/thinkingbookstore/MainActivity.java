55b3e5ac19fce6084c59f5ab7f9067c5c3c697ae
888pocket <888pocket888@gmail.com>
2020-11-17 17:28:29 +0900

    trans all

--- app/src/main/java/com/code3/thinkingbookstore/MainActivity.java
+++ app/src/main/java/com/code3/thinkingbookstore/MainActivity.java
@@ -1,15 +1,42 @@
 package com.code3.thinkingbookstore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+    HomeFragment homeFragment = new HomeFragment();
+    ListFragment listFragment = new ListFragment();
+    UploadFragment likeFragment = new UploadFragment();
+    MypageFragment myPageFragment = new MypageFragment();
+
     FirebaseUser user;
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container, homeFragment).commit();
+                return true;
+            case R.id.menu_list:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container, listFragment).commit();
+                return true;
+            case R.id.menu_like:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container, likeFragment).commit();
+                return true;
+            case R.id.menu_myPage:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container, myPageFragment).commit();
+                return true;
+        }
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +49,13 @@ public class MainActivity extends AppCompatActivity {
         } else {    // 로그인 되어있으면
 
         }
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bn_bottom_navi);
+        bottomNavigationView.setItemIconTintList(null);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, homeFragment).commit(); //특정 Fragment를 첫화면으로 설정하고 싶을 경우 homeFragment->해당Fragment로 변경경
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
     }
 }
