6c3dcaab7727fcd0d17cd33eb90fc51d7d239d24
seong hwan kim <ksh9409255@gmail.com>
2020-11-22 14:00:04 +0900

    홈화면구성

---
+++ app/src/main/java/com/code3/thinkingbookstore/RecyclerHomeAdapter.java
@@ -0,0 +1,59 @@
+package com.code3.thinkingbookstore;
+
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerHomeAdapter extends RecyclerView.Adapter<RecyclerHomeAdapter.ItemViewHolder>{
+
+    public ArrayList<RecyclerHomeData> listData = new ArrayList<>();
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_home_item, parent, false);
+        return new ItemViewHolder(view);
+    }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerHomeAdapter.ItemViewHolder holder, int position) {
+        holder.onBind(listData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+    void addItem(RecyclerHomeData recyclerHomeData) {
+        listData.add(recyclerHomeData);
+    }
+
+    static class ItemViewHolder extends RecyclerView.ViewHolder {
+        private ImageView contentImageView;
+
+        ItemViewHolder(View itemView) {
+            super(itemView);
+
+            contentImageView = itemView.findViewById(R.id.content_image_view);
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        void onBind(RecyclerHomeData listData) {
+            contentImageView.setImageResource(listData.getImageView());
+            contentImageView.setClipToOutline(true);
+        }
+    }
+}
---
+++ app/src/main/java/com/code3/thinkingbookstore/RecyclerHomeData.java
@@ -0,0 +1,7 @@
+package com.code3.thinkingbookstore;
+
+public class RecyclerHomeData {
+    int imageView;
+    public int getImageView(){return imageView;}
+    public void setImageView(int imageView){this.imageView=imageView;}
+}
