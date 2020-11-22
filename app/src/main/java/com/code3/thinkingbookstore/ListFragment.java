fdedb2d6bf4866bdeb45ecb6426c0f6b29cc4571
seong hwan kim <ksh9409255@gmail.com>
2020-11-22 15:04:24 +0900

    책방화면구현

--- app/src/main/java/com/code3/thinkingbookstore/ListFragment.java
+++ app/src/main/java/com/code3/thinkingbookstore/ListFragment.java
@@ -3,62 +3,64 @@ package com.code3.thinkingbookstore;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Arrays;
+import java.util.List;
+
 public class ListFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ListFragment newInstance(String param1, String param2) {
-        ListFragment fragment = new ListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private RecyclerView recyclerViewBook;
+    private RecyclerView recyclerViewDown;
+    private RecyclerListAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        DataInit(view);
+        return view;
+    }
+    private void DataInit(View view){
+        recyclerViewBook = (RecyclerView) view.findViewById(R.id.recyclerView_book_list);
+        recyclerViewDown = (RecyclerView) view.findViewById(R.id.recyclerView_book_down);
+        recyclerViewDown.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(recyclerViewDown.getContext(),LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewDown.setLayoutManager(layoutManager);
+        adapter = new RecyclerListAdapter();
+        getData();
+        recyclerViewDown.setAdapter(adapter);
+        recyclerViewDown.setItemAnimator(new DefaultItemAnimator());
+
+        recyclerViewBook.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(recyclerViewBook.getContext(),LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewBook.setLayoutManager(layoutManager);
+        adapter = new RecyclerListAdapter();
+        getData();
+        recyclerViewBook.setAdapter(adapter);
+        recyclerViewBook.setItemAnimator(new DefaultItemAnimator());
+    }
+    private void getData(){
+        adapter.listData.clear();
+        List<Integer> listContentImageView = Arrays.asList(
+                R.drawable.sample_book,
+                R.drawable.sample_book,
+                R.drawable.sample_book,
+                R.drawable.sample_book,
+                R.drawable.sample_book,
+                R.drawable.sample_book,
+                R.drawable.sample_book);
+        for (int i = 0; i < listContentImageView.size(); i++) {
+            RecyclerListData data = new RecyclerListData();
+            data.setImageView(listContentImageView.get(i));
+            adapter.addItem(data);
+        }
+        adapter.notifyDataSetChanged();
     }
 }
