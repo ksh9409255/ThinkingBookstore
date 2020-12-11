9e1b08d3aaad94dcff9fa4b96064cae4bc25ef37
888pocket <dndmdo8@gmail.com>
2020-12-12 01:05:48 +0900

    user can add comment on picture

--- app/src/main/java/com/code3/thinkingbookstore/PhotoEditActivity.java
+++ app/src/main/java/com/code3/thinkingbookstore/PhotoEditActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -11,8 +12,11 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,20 +27,23 @@ import ja.burhanrashid52.photoeditor.PhotoEditor;
 import ja.burhanrashid52.photoeditor.PhotoEditorView;
 import ja.burhanrashid52.photoeditor.ViewType;
 
+import static android.view.View.VISIBLE;
+
 public class PhotoEditActivity extends AppCompatActivity {
     Button complete;
     Button[] Buttons = new Button[5];
     Integer[] ButtonIDs = {R.id.editcolor1, R.id.editcolor2, R.id.editcolor3, R.id.editcolor4, R.id.editcolor5};
     int[] colors = {Color.BLACK, Color.RED, Color.BLUE, Color.YELLOW, Color.GREEN};
-    ImageButton getPho, getSen, close;
+    ImageButton getPho, getSen, close, getEdit;
     TextView iseditable;
     private Toast mToast;
+    private LinearLayout mLayout;
+    private EditText edit;
 
     private static final int REQUEST_CODE = 0;
     private static final int REQUEST_CODE_SENTENCE = 1;
     PhotoEditor mPhotoEditor;
     PhotoEditorView mPhoto;
-    private String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +83,35 @@ public class PhotoEditActivity extends AppCompatActivity {
                 finish();
             }
         });
+        getEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLayout.setVisibility(VISIBLE);
+                edit.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        edit.setFocusableInTouchMode(true);
+                        edit.requestFocus();
+
+                        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                        imm.showSoftInput(edit,0);
+                    }
+                });
+            }
+        });
+    }
+
+    public void editOnClick(View v) {
+        try {
+            mPhotoEditor.addText(edit.getText().toString(), Color.BLACK);
+        } catch(Exception e) {
+
+        }
+        mLayout.setVisibility(View.GONE);
+        edit.setText("");
+        InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(edit.getWindowToken(), 0);
     }
 
     private void bindView() {
@@ -86,6 +122,10 @@ public class PhotoEditActivity extends AppCompatActivity {
         getSen = (ImageButton)findViewById(R.id.getsentence);
         close = (ImageButton)findViewById(R.id.backbtn_edit);
         iseditable = (TextView)findViewById(R.id.iseditable);
+        mLayout = (LinearLayout)findViewById(R.id.edit_back);
+        edit = (EditText)findViewById(R.id.edit_edit);
+        getEdit = (ImageButton)findViewById(R.id.getedittext);
+        complete = (Button)findViewById(R.id.complete_edit);
 
         mPhoto = findViewById(R.id.photoeditor);
     }
@@ -153,10 +193,13 @@ public class PhotoEditActivity extends AppCompatActivity {
                         @SuppressLint("MissingPermission")
                         @Override
                         public void onClick(View view) {
+                            Log.e("button", "pressed");
                             mPhotoEditor.saveAsFile("/data/data/com.code3.thinkingbookstore/files/temp.jpg", new PhotoEditor.OnSaveListener() {
                                 @Override
                                 public void onSuccess(@NonNull String imagePath) {
                                     Log.e("PhotoEditor", "Image Saved Successfully");
+                                    Intent intent = getIntent();
+                                    setResult(RESULT_OK, intent);
                                     makeToast("편집 완료");
                                     finish();
                                 }
