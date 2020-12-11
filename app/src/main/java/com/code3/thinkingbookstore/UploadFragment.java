54a4c7d9eee6055d16b4566fb6c3d0258f0ef137
seong hwan kim <ksh9409255@gmail.com>
2020-12-12 02:25:32 +0900

    업로드화면

--- app/src/main/java/com/code3/thinkingbookstore/UploadFragment.java
+++ app/src/main/java/com/code3/thinkingbookstore/UploadFragment.java
@@ -1,64 +1,97 @@
 package com.code3.thinkingbookstore;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UploadFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.InputStream;
+
+import ja.burhanrashid52.photoeditor.OnPhotoEditorListener;
+import ja.burhanrashid52.photoeditor.PhotoEditor;
+import ja.burhanrashid52.photoeditor.PhotoEditorView;
+import ja.burhanrashid52.photoeditor.ViewType;
+
+import static android.app.Activity.RESULT_OK;
+
 public class UploadFragment extends Fragment {
+    private ImageView uploadPic;
+    private Button btnEdit;
+    private Button btnUpload;
+    private EditText uploadDetail;
+    private EditText uploadWriter;
+    private EditText uploadBookName;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    FirebaseUser user;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    String imageView = "https://firebasestorage.googleapis.com/v0/b/thinkingbookshop.appspot.com/o/post%2Fempty.png?alt=media&token=3b7e50a9-188c-488d-9cd8-0a6b0a837320";
 
-    public UploadFragment() {
-        // Required empty public constructor
-    }
+    private RecyclerHomeData data;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UploadFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UploadFragment newInstance(String param1, String param2) {
-        UploadFragment fragment = new UploadFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload, container, false);
+        View view = inflater.inflate(R.layout.fragment_upload, container, false);
+
+        uploadPic = (ImageView)view.findViewById(R.id.upload_content_image_view);
+        btnEdit = (Button)view.findViewById(R.id.upload_pic_edit);
+        btnUpload = (Button)view.findViewById(R.id.btn_upload);
+        uploadDetail = (EditText)view.findViewById(R.id.upload_edit_detail);
+        uploadWriter = (EditText)view.findViewById(R.id.upload_writer_edit);
+        uploadBookName = (EditText)view.findViewById(R.id.upload_book_name_edit);
+
+        btnEdit.setOnClickListener(l->{
+            Intent intent = new Intent(getContext(),PhotoEditActivity.class);
+            startActivityForResult(intent,2000);
+        });
+
+        btnUpload.setOnClickListener(l->{
+            String detail = uploadDetail.getText().toString().trim();
+            String writer = uploadWriter.getText().toString().trim();
+            String bookName = uploadBookName.getText().toString().trim();
+            user = FirebaseAuth.getInstance().getCurrentUser();
+
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            data = new RecyclerHomeData(imageView,user.getDisplayName(),detail,writer,bookName);
+            DatabaseReference databaseReference = database.getReference("post_list");
+            databaseReference.push().setValue(data);
+        });
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==2000){
+            if(resultCode==RESULT_OK){
+                imageView="/data/data/com.code3.thinkingbookstore/files/temp.jpg";
+                Glide.with(getView()).load(imageView).into(uploadPic);
+            }
+        }
     }
 }
