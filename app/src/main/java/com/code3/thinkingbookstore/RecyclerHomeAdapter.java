e5a1c8a1ebc8d8698ed8ded4206ae3e0fba92cba
seong hwan kim <ksh9409255@gmail.com>
2020-12-12 04:12:26 +0900

    업로드 기능완료

--- app/src/main/java/com/code3/thinkingbookstore/RecyclerHomeAdapter.java
+++ app/src/main/java/com/code3/thinkingbookstore/RecyclerHomeAdapter.java
@@ -14,6 +14,7 @@ import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import java.util.ArrayList;
 
